1a2f0c5cbe59021d10a89cd8553c0abbc161f718
gauravmatta <gaurav.matta@gmail.com>
2018-03-26 01:52:01 +0530

    location done

--- location.java
+++ location.java
@@ -3,12 +3,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URI;
 import java.util.Random;
-//import javax.ws.rs.core.UriBuilder;
-import javax.xml.ws.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,14 +21,13 @@ import javax.xml.ws.Response;
  * @author Gaurav Matta
  */
 public class location {
-    public static void main(String args[]) throws MalformedURLException
+    public static void main(String args[])
     {
         int latMin=-85;
         int latmax=85;
         int longMin=-180;
         int longmax=180;
         int [][] locations=new int [10][2];
-        
         for (int[] location : locations) {
             Random r = new Random();
             int randLat=r.nextInt(latmax - latMin + 1) + latMin;
@@ -39,14 +38,14 @@ public class location {
         
         for(int i = 0; i<locations.length; i++) 
         {
+            System.out.println("At Longitude and Latitude");
             for(int j=0;j<locations[i].length;j++)
             {
-                System.out.println("At Position "+i+" "+j);
                 System.out.println(locations[i][j]);
             }
+            
             try
             {
-                System.out.println("Finding Address...\n");
                 URL url=new URL("https://maps.googleapis.com/maps/api/geocode/json?latlng="+locations[i][0]+","+locations[i][1]+"&key=AIzaSyDkUmLA-5-n5J2wE0QdrL9IQb5EFdon5qM");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
@@ -55,21 +54,57 @@ public class location {
                 {
                     throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
                 }
-                
+                StringBuilder str = new StringBuilder();
                 BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
                 String output;
-                System.out.println("Output from Server .... \n");
                 while ((output = br.readLine()) != null) 
                 {
-                    System.out.println(output);	
+                    str.append(output).append("\n");
                 }
                 conn.disconnect();
+                 
+                String result;
+                result=str.toString();
+                JSONParser parser = new JSONParser();
+                try
+                {
+                    Object obj = parser.parse(result);
+                    JSONObject obj2 = (JSONObject)obj;
+                    JSONArray results=(JSONArray)obj2.get("results");
+                    if(!(results.isEmpty()))
+                    {
+                        JSONObject obj3=(JSONObject)results.get(0);
+                        JSONArray address_components=(JSONArray) obj3.get("address_components");
+                        if(!(address_components.isEmpty()))
+                        {
+                            for(Object obj4:address_components)
+                            {
+                               JSONObject obj5=(JSONObject) obj4;
+                               JSONArray types=(JSONArray)obj5.get("types");
+                               if(types.contains("country"))
+                               {
+                                   Object long_name=obj5.get("long_name");
+                                   System.out.println("You are in "+long_name);
+                               }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("You are in Water");
+                    }
+                    
+                }
+                catch(ParseException e)
+                {
+                    System.out.println("Error"+e);
+                }
             }
             catch(IOException | RuntimeException e)
             {
                 System.out.println("Error"+e);
-//                e.printStackTrace();
             }
-        }        
+            
+        }
     }
 }
