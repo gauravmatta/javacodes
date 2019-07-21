85aa988e74768f52e745c486d03524d705d564be
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2019-07-21 21:58:22 +0530

    added Java.util.Scanner example

---
+++ PojoClasses/src/ScannerTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+public static void main(String args[])
+{
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter a Number");
+	int a=scanner.nextInt();
+	System.out.println(a);
+	char c=scanner.next().charAt(0);
+	System.out.println(c);
+}
+}
---
+++ PojoClasses/src/firstapp/firstapp/testapp/testclass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapp.testapp;
+
+/**
+ *
+ * @author Gaurav Matta
+ */
+public class testclass {
+    
+}
