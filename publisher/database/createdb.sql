7a1cdca522295a53e0806f28df5876b66aeed812
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2019-08-08 23:52:35 +0530

    added shirts and customers tables

--- publisher/database/createdb.sql
+++ publisher/database/createdb.sql
@@ -1,10 +1,23 @@
-create table news_item
+create table IF NOT EXISTS news_item
 (
 id integer primary key,
 title text not null,
 url text not null
 );
-create table sequence
+create table IF NOT EXISTS customers
+(
+id integer primary key,
+name text not null,
+age integer
+);
+create table IF NOT EXISTS shirts
+(
+id integer primary key,
+color text not null,
+brand text not null,
+customer_id integer
+);
+create table IF NOT EXISTS sequence
 (
 next_value integer
 );
