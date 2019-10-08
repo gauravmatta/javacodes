907cc3fce7e741a053ee69423daf4b52174a8b5d
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2019-10-08 15:28:18 +0530

    updated Pojo Classes

--- PojoClasses/src/javaimplant/jdbc/MySQLAccess.java
+++ PojoClasses/src/javaimplant/jdbc/MySQLAccess.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 import javax.transaction.xa.Xid;
 
 /**
