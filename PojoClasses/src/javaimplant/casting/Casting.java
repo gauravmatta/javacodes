9695726f543681188c896a7df55396791103f0a7
703199444 <703199444@703199444L.ind.ge.ad>
2019-10-09 17:43:14 +0530

    added Casting

---
+++ PojoClasses/src/javaimplant/casting/Casting.java
@@ -0,0 +1,14 @@
+package javaimplant.casting;
+
+public class Casting {
+	
+	public static void main(String args[])
+	{
+		byte byteVar = 42;
+		short shortVar = byteVar;
+		int intVar = shortVar;
+		long longVar = intVar;
+		float floatVar = longVar;
+		double doubleVar = floatVar;
+	}
+}
