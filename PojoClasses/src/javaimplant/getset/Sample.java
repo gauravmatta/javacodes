d008c3e9816f83796181af5b050cae98a6be3e52
Gaurav <Gaurav@local>
2019-11-06 15:12:30 +0530

    Added getter and setter

---
+++ PojoClasses/src/javaimplant/getset/Sample.java
@@ -0,0 +1,26 @@
+package javaimplant.getset;
+
+public class Sample {
+	private String name;
+	private int age;
+	boolean gender;
+	
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
