86b66395eab911051ad394509ede66c48f5b363a
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2019-12-16 00:57:15 +0530

    Added publish webservice
    Signed-off-by: Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>

--- wiki/src/main/java/wiki/web/PublishPageServlet.java
+++ wiki/src/main/java/wiki/web/PublishPageServlet.java
@@ -1,6 +1,12 @@
 package wiki.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.Socket;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -11,6 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
 
 import wiki.data.Page;
 import wiki.data.PageDAO;
@@ -41,6 +52,64 @@ public class PublishPageServlet extends HttpServlet {
 		}
 		req.setAttribute("wikipage", page);
 		jsp.forward(req, resp);
+	}
+	
+	private String publish(Page page) throws IOException
+	{
+		logger.debug("publish()");
+		String pageName = page.getName();
+		String pageUrl = "http://localhost:8080/wiki/view/" + pageName;
+		SAXBuilder builder = new SAXBuilder();
+		Element titleElement = new Element("title");
+		titleElement.addContent(pageName);
+		Element linkElement = new Element("link");
+		linkElement.addContent(pageUrl);
+		Element root = new Element("item");
+		root.addContent(titleElement);
+		root.addContent(linkElement);
+		Document document = new Document(root);
+		StringWriter sw = new StringWriter();
+		XMLOutputter outputter = new XMLOutputter();
+		outputter.output(document, sw);
+		String docString = sw.toString();
+		byte[] docBytes = docString.getBytes("UTF-8");
+		String contentLengthHeader = "Content-length: " + docBytes.length + "\r\n";
+		String contentTypeHeader = "Content-type: text/xml\r\n";
+		String hostHeader = "Host: localhost\r\n";
+		String connectionHeader = "Connection: close\r\n";
+		String requestLine = "POST /newsfeed/publish HTTP/1.1\r\n";
+		Socket socket = new Socket("localhost", 8080);
+		OutputStream os = socket.getOutputStream();
+		os.write(requestLine.getBytes("US-ASCII"));
+		os.write(hostHeader.getBytes("US-ASCII"));
+		os.write(contentTypeHeader.getBytes("US-ASCII"));
+		os.write(contentLengthHeader.getBytes("US-ASCII"));
+		os.write(connectionHeader.getBytes("US-ASCII"));
+		os.write("\r\n".getBytes("US-ASCII"));
+		os.write(docBytes);
+		os.flush();
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		// Read through header lines.
+		while(true)
+		{
+			String line = br.readLine();
+			if (line.length() == 0) break;
+		}
+		Document responseDoc = null;
+		try
+		{
+			responseDoc = builder.build(br);
+		}
+		catch (JDOMException e)
+		{
+			throw new RuntimeException(e);
+		}
+		Element idElement = responseDoc.getRootElement();
+		String id = idElement.getText();
+		br.close();
+		return id;
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
@@ -84,10 +153,5 @@ public class PublishPageServlet extends HttpServlet {
 		// Update page.
 		pageDAO.update(page);
 		resp.sendRedirect("../view/" + page.getName());
-	}
-	
-	private String publish(Page page) throws IOException
-	{
-		return "3"; // Pretend that publish service returned an id of 3.
 	}
 }
