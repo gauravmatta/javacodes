5214cc9f18d2f6b00f051d4135e16bed19e2fd90
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2020-01-22 23:07:11 +0530

    Added Test Suit Example

--- PojoClasses/src/javaimplant/junitTest/JunitTestSuite.java
+++ PojoClasses/src/javaimplant/junitTest/JunitTestSuite.java
@@ -3,6 +3,9 @@ package javaimplant.junitTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
 //JUnit Suite Test
 @RunWith(Suite.class)
 
@@ -11,5 +14,11 @@ import org.junit.runners.Suite;
 })
 
 public class JunitTestSuite {
-
+	public static void main(String[] a) {
+	      // add the test's in the suite
+	      TestSuite suite = new TestSuite(TestJunit1.class, TestJunit2.class, TestJunit3.class );
+	      TestResult result = new TestResult();
+	      suite.run(result);
+	      System.out.println("Number of test cases = " + result.runCount());
+	   }
 }
