31c727f5cc18d8c40ac7d5cc3e10a92c691107e2
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2020-03-15 22:42:38 +0530

    Added login action

--- SocialNetwork/src/main/java/com/javaimplant/socialnetwork/action/LoginAction.java
+++ SocialNetwork/src/main/java/com/javaimplant/socialnetwork/action/LoginAction.java
@@ -1,5 +1,8 @@
 package com.javaimplant.socialnetwork.action;
 
 public class LoginAction {
-
+	public String execute() {
+		System.out.println("We are executing login action!");
+		return "success";
+	}
 }
