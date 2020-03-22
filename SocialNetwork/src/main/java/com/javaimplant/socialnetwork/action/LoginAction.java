cc6baa5979d02df142370b5f9a65abf20c8b9695
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2020-03-23 00:33:47 +0530

    Updated Login Action

--- SocialNetwork/src/main/java/com/javaimplant/socialnetwork/action/LoginAction.java
+++ SocialNetwork/src/main/java/com/javaimplant/socialnetwork/action/LoginAction.java
@@ -1,8 +1,32 @@
 package com.javaimplant.socialnetwork.action;
 
-public class LoginAction {
+import com.javaimplant.socialnetwork.model.User;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	private User user;
+	
+	@Override
+	public void validate() {
+		
+	}
+
+	@Override
 	public String execute() {
 		System.out.println("We are executing login action!");
-		return "success";
+		System.out.println(user.getUserName());
+		System.out.println(user.getPassword());
+		return SUCCESS;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 }
