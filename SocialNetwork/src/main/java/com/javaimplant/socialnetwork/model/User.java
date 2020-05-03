b6e2953957f96989541528bd3a2ecab6137db659
Gaurav Matta <gaurav.matta@gmail.com>
2020-05-03 20:43:26 +0530

    updated project

--- SocialNetwork/src/main/java/com/javaimplant/socialnetwork/model/User.java
+++ SocialNetwork/src/main/java/com/javaimplant/socialnetwork/model/User.java
@@ -1,19 +1,12 @@
 package com.javaimplant.socialnetwork.model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
