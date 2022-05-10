dfe027415fbcb6f10378172f5f79d4aa78b72953
Gaurav Matta <gaurav.matta@gmail.com>
2022-05-10 14:02:44 +0530

    Updated Test Case

--- PojoClasses/src/javaimplant/junitTest/SimpleCalculatorTest.java
+++ PojoClasses/src/javaimplant/junitTest/SimpleCalculatorTest.java
@@ -10,6 +10,14 @@ public class SimpleCalculatorTest {
 	public void twoPlustwoShouldEqualFour() {
 		SimpleCalculator calculator= new SimpleCalculator();
 		assertEquals(4,calculator.add(2, 2));
+		assertNotEquals(5,calculator.add(1, 1));
+		assertTrue(calculator.equals(calculator));
+	}
+	
+	@Test
+	public void threePlusSevenEqualsTen() {
+		SimpleCalculator calculator = new SimpleCalculator();
+		assertEquals(10,calculator.add(3, 7));
 	}
 	
 	@Test
