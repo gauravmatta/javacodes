0c4f79bfa0580f3b05f738b32768f5f1fb32a955
Gaurav <Gaurav@host.docker.internal>
2022-08-22 20:01:21 +0530

    Updated Reflexion API

--- reflexion_api/src/reflexion_api/Employee.java
+++ reflexion_api/src/reflexion_api/Employee.java
@@ -1,8 +1,39 @@
 package reflexion_api;
 
 import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 
-public abstract class Employee implements Serializable, Cloneable {
-
-
+public class Employee implements Serializable, Cloneable {
+	public static int eno=1;
+	static String ename = "Gaurav";
+	public static String eaddr = "Delhi";
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(int eno, String ename, String eaddr) throws ClassCastException,ArithmeticException {
+		
+	}
+	
+	public Employee(int eno, String ename) throws RemoteException,SQLException {
+		
+	}
+	
+	public Employee(int eno) throws InterruptedException {
+		
+	}
+	
+	public void add(String eno, String ename, String eaddr) throws ClassNotFoundException, ClassCastException {
+		
+	}
+	
+	public String search(int eno) throws ArithmeticException, InterruptedException {
+		return null;
+	}
+	
+	public void delete(int eno) throws SQLException {
+		
+	}
 }
--- reflexion_api/src/reflexion_api/Test.java
+++ reflexion_api/src/reflexion_api/Test.java
@@ -1,5 +1,8 @@
 package reflexion_api;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
@@ -14,6 +17,54 @@ public class Test {
 		Arrays.asList(cl).stream().map(cls -> cls.getName()+" ").forEach(System.out::println);
 		int i = c.getModifiers();
 		System.out.println("Access Modifiers: "+ Modifier.toString(i));
+		
+		Employee e = new Employee();
+		c=e.getClass();
+		Field[] flds = c.getDeclaredFields();
+		Arrays.asList(flds).stream().map(f -> {
+			try {
+				return f.getName()+ " " + f.getType().getName()+ " " + Modifier.toString(f.getModifiers())+ " " +f.get(f);
+			} catch (IllegalArgumentException | IllegalAccessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return null;
+		}).forEach(System.out::println);
+		
+		
+		System.out.println("Methods=============>");
+		c=Employee.class;
+		Method[] mthd = c.getDeclaredMethods();
+		Arrays.asList(mthd).stream()
+		.map(m -> {
+			Class[] pts = m.getParameterTypes();
+			for (Class c1:pts)
+			{
+				System.out.println(c1.getName()+" ");
+			}
+			Class[] exTypes = m.getExceptionTypes();
+			for (Class c1:exTypes)
+			{
+				System.out.println(c1.getName()+" ");
+			}
+			return m.getName() + " " +m.getReturnType().getName()+" "+Modifier.toString(m.getModifiers());
+			})
+		.forEach(System.out::println);
+		System.out.println("Constructors=============>");
+		Constructor[] con = c.getDeclaredConstructors();
+		Arrays.asList(con).stream().map(co -> {
+			Class[] pts = co.getParameterTypes();
+			for (Class c1:pts)
+			{
+				System.out.println(c1.getName()+" ");
+			}
+			Class[] exTypes = co.getExceptionTypes();
+			for (Class c1:exTypes)
+			{
+				System.out.println(c1.getName()+" ");
+			}
+			return co.getName() + " " + Modifier.toString(co.getModifiers());
+		}).forEach(System.out::println);
 	}
 
 }
