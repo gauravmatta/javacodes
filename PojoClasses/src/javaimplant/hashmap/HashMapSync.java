97c3dfb9d172ed3ace86a2941a69d160dcbeed75
Gaurav <Gaurav@host.docker.internal>
2022-09-02 16:56:44 +0530

    Added Concurrent Hash Map Example

--- PojoClasses/src/javaimplant/hashmap/HashMapSync.java
+++ PojoClasses/src/javaimplant/hashmap/HashMapSync.java
@@ -24,6 +24,29 @@ public class HashMapSync {
 		concurrentHashMap.put("C", "Ruby");
 		
 		System.out.println(concurrentHashMap.get("A"));
+		
+		//Synchronized Hash Map
+		Map<String, Integer> hashMap = new HashMap<>();
+		Map<String, Integer> syncMap2 = Collections.synchronizedMap(hashMap);
+		MapHelper mapHelper = new MapHelper(syncMap2);
+		MapHelper2 mapHelper2 = new MapHelper2(syncMap2);
+		MapHelper3 mapHelper3 = new MapHelper3(syncMap2);
+		MapHelper4 mapHelper4 = new MapHelper4(syncMap2);
+		
+		for(Map.Entry<String,Integer> e: syncMap2.entrySet()) {
+			System.out.println(e.getKey() + "=" + e.getValue());
+		}
+		
+		//Concurrent Hashmap
+		ConcurrentHashMap<String, Integer> concurrentHashMap2 = new ConcurrentHashMap<>();
+		MapHelper smapHelper = new MapHelper(concurrentHashMap2);
+		MapHelper2 smapHelper2 = new MapHelper2(concurrentHashMap2);
+		MapHelper3 smapHelper3 = new MapHelper3(concurrentHashMap2);
+		MapHelper4 smapHelper4 = new MapHelper4(concurrentHashMap2);
+		
+		for(Map.Entry<String,Integer> e: concurrentHashMap2.entrySet()) {
+			System.out.println(e.getKey() + "=" + e.getValue());
+		}
 	}
 
 }
