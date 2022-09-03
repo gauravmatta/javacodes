d48615e603a96032fd3730543f31e79517a6188c
Gaurav <Gaurav@host.docker.internal>
2022-09-03 11:24:59 +0530

    Added custom Thread Pool

--- PojoClasses/src/javaimplant/threads/RunnableMain.java
+++ PojoClasses/src/javaimplant/threads/RunnableMain.java
@@ -2,12 +2,13 @@ package javaimplant.threads;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class RunnableMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ExecutorService executorService= Executors.newFixedThreadPool(5);
-		for(int i=0;i<10;i++) {
+		for(int i=0;i<=10;i++) {
 			Runnable runnable=new MyRunnable("myWorkerThread"+i);
 			executorService.execute(runnable);
 		}
@@ -16,6 +17,21 @@ public class RunnableMain {
 		while(!executorService.isTerminated()) {
 			System.out.println("All requests completed Successfully");
 		}
+		
+		
+		//Custom Thread Pool Implementation Example
+		ThreadPool threadPool = new ThreadPool(3, 10);
+		for(int i=0;i<10;i++) {
+			int taskNo = i;
+			threadPool.execute(() -> {
+				String msgString = Thread.currentThread().getName()+": " + taskNo;
+				System.out.println(msgString);
+			});
+		}
+		
+		threadPool.waitUntillAllTasksFinished();
+		threadPool.stop();
+		//Custom Thread Pool Implementation Example
 	}
 
 }
