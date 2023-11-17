a56541097008277f34c6dbc10d396f1370be88fe
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-11-17 12:34:11 +0530

    added servlet project JobPortal

---
+++ JobPortal/src/main/java/com/springimplant/JobPortal/servlet/TestServlet.java
@@ -0,0 +1,28 @@
+package com.springimplant.JobPortal.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -2387773506194580372L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().append("Served at: ").append(req.getContextPath());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+	
+
+}
