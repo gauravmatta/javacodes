dd7f7a01302941212c4770c6e5f81774d7e5f37b
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-11-17 16:04:54 +0530

    Added DAO

---
+++ JobPortal/src/main/java/com/springimplant/jobportal/dao/JobPostingDAO.java
@@ -0,0 +1,27 @@
+package com.springimplant.jobportal.dao;
+
+import java.util.List;
+
+import com.springimplant.jobportal.entity.JobPosting;
+
+import lombok.Data;
+
+@Data
+public class JobPostingDAO {
+
+	public JobPosting getJobPostingById(int id) {
+		return null;
+	}
+	
+	public void insertJobPosting(JobPosting posting) {
+		
+	}
+	
+	public void removeJobPosting(int jobPostId) {
+		
+	}
+	
+	public List<JobPosting> getAllJobPostings() {
+		return null;
+	}
+}
