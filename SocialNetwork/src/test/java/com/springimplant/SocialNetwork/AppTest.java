834e40f856344ac359c444383c4d8f289fbd7a4d
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-11-18 11:57:11 +0530

    updated project

---
+++ SocialNetwork/src/test/java/com/springimplant/SocialNetwork/AppTest.java
@@ -0,0 +1,20 @@
+package com.springimplant.SocialNetwork;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
