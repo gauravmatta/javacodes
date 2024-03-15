b7ef881e3eca2915859d8031cb7eafd797d8f133
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-03-15 12:43:43 +0530

    added doget method

--- servletdemo/src/main/java/com/javaimplant/servletdemo/MyHttpServlet.java
+++ servletdemo/src/main/java/com/javaimplant/servletdemo/MyHttpServlet.java
@@ -1,9 +1,19 @@
 package com.javaimplant.servletdemo;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class MyHttpServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6668592872752379842L;
-
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("I am in doGet() method");
+		super.doGet(req, resp);
+	}
 }
