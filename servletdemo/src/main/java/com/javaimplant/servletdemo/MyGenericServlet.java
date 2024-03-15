86eb2e47b05147f7c34de2ac3d5af5e2eacba95c
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-03-15 12:12:34 +0530

    added Generic and HttpServlet

---
+++ servletdemo/src/main/java/com/javaimplant/servletdemo/MyGenericServlet.java
@@ -0,0 +1,20 @@
+package com.javaimplant.servletdemo;
+
+import java.io.IOException;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class MyGenericServlet extends GenericServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ servletdemo/src/main/java/com/javaimplant/servletdemo/MyHttpServlet.java
@@ -0,0 +1,9 @@
+package com.javaimplant.servletdemo;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class MyHttpServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6668592872752379842L;
+
+}
