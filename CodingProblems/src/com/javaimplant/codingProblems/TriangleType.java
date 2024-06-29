9d15f17ddf640aded1c9e3175a8f1f7801f06e5c
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-29 14:01:51 +0530

    Added Triangle Type

---
+++ CodingProblems/src/com/javaimplant/codingProblems/TriangleType.java
@@ -0,0 +1,27 @@
+/*
+ * In the assembly line, the factory assembles three parts ' a ' ' b ' ' c ' of a triangle toy. A
+ * valid toy is one where the three sides can connect at their endpoints.
+ * There are two forms of valid triangles to identify.
+ * If 2 parts are of equal length, the form is 'Isosceles'
+ * If all 3 parts are of equal length, the form is 'Equilateral'
+ * If a toy is not valid or is not one of those 2 forms, it is 'None of these'.
+ * 
+ * Example
+ * 
+ * triangleToy=['2 2 1;'3 3 3','3 4 5','1 1 3']
+ * The first triangle is valid and has 2 equal parts, so the form is 'Isosceles'
+ * The second is valid and has 3 equal parts, so the form is 'Equilateral'
+ * The third is valid but not one of the two specific forms.
+ * The fourth is not valid.
+ * The result is ["Isosceles", "Equilateral", "None of these", "None of these"]
+ */
+package com.javaimplant.codingProblems;
+
+public class TriangleType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
