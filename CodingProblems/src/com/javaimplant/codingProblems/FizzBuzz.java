3c1cf96e07809d0de156f55ea12469c5d8ce249e
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-29 13:56:21 +0530

    Added odd even transform

--- CodingProblems/src/com/javaimplant/codingProblems/FizzBuzz.java
+++ CodingProblems/src/com/javaimplant/codingProblems/FizzBuzz.java
@@ -19,7 +19,7 @@ public class FizzBuzz {
 		for (int i = 1; i <= n; ++i) { 
 			
 			// Check if i is divisible by both 3 and 5 
-			if (i % 3 == 0 && i % 5 == 0) { 
+			if (i % 3 == 0 && i % 5 == 0) {
 				
 				// Add "FizzBuzz" to the result list 
 				result.add("FizzBuzz"); 
