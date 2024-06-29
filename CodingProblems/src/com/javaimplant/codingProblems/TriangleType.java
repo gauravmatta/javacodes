28c75f88d455b97fda7a6628129f6ee7c81c9bf1
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-29 15:52:14 +0530

    Added Triangle Type

--- CodingProblems/src/com/javaimplant/codingProblems/TriangleType.java
+++ CodingProblems/src/com/javaimplant/codingProblems/TriangleType.java
@@ -8,7 +8,7 @@
  * 
  * Example
  * 
- * triangleToy=['2 2 1;'3 3 3','3 4 5','1 1 3']
+ * triangleToy=['2 2 1','3 3 3','3 4 5','1 1 3']
  * The first triangle is valid and has 2 equal parts, so the form is 'Isosceles'
  * The second is valid and has 3 equal parts, so the form is 'Equilateral'
  * The third is valid but not one of the two specific forms.
@@ -17,11 +17,48 @@
  */
 package com.javaimplant.codingProblems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TriangleType {
+	
+	private static String[] split;
+
+	public static List<String> triangleType(List<String> arr) {
+	List<String> result =	arr.stream().map(a->{
+		split = a.split(" ");
+		if(split.length !=3) {
+			return "None of These";
+		}
+		else if((Integer.valueOf(split[0]) == Integer.valueOf(split[1])) && (Integer.valueOf(split[1]) == Integer.valueOf(split[2]))) {
+			return "Equilateral";
+		}
+		else if((Integer.valueOf(split[0]) == Integer.valueOf(split[1])) && (Integer.valueOf(split[2])<(Integer.valueOf(split[0]) + Integer.valueOf(split[1])))){
+			return "Isosceles";
+		}
+		else if((Integer.valueOf(split[1]) == Integer.valueOf(split[2])) && (Integer.valueOf(split[0])<(Integer.valueOf(split[1]) + Integer.valueOf(split[2])))) {
+			return "Isosceles";
+		}
+		else if((Integer.valueOf(split[0]) == Integer.valueOf(split[2])) && (Integer.valueOf(split[1])<(Integer.valueOf(split[0]) + Integer.valueOf(split[2])))) {
+			return "Isosceles";
+		}
+		else {
+			return "None of These";
+		}
+		}).collect(Collectors.toList());
+		return result;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> str = new ArrayList<>() {{
+			  add("2 2 1");
+              add("3 3 3");
+              add("3 4 5");
+              add("1 1 3");
+		}};
+		List<String> result = triangleType(str);
+		result.forEach(r -> System.out.println(r));
 	}
 
 }
