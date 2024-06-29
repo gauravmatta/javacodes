97409939e7185fbd09d4d684afa2d09aedfa1cad
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-29 17:27:17 +0530

    Added Json Difference

---
+++ CodingProblems/src/com/javaimplant/codingProblems/JsonDifference.java
@@ -0,0 +1,17 @@
+/*
+ * implement a simple prototype service to find the difference between two json objects
+ * To keep the prototype simple a Json will have only key value pairs with no nested elements.
+ * Find the keys for whose values are different.
+ * If a key is present in only one of the Json's it should not be considered in result. 
+ * The list of keys should be sorted in lexicographically ascending order.
+ */
+package com.javaimplant.codingProblems;
+
+public class JsonDifference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
