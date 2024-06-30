9205ab0119c7cca8e17e476a130e28d530cbf6c8
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-30 18:14:17 +0530

    Added Lexigraphically Smallest String

---
+++ CodingProblems/src/com/javaimplant/codingProblems/ibm/LexicographicallySmallestString.java
@@ -0,0 +1,26 @@
+/*
+ * Two strings are given, word and substr. Some of the characters in word are a question mark (?).
+ * ﻿Find the lexicographically smallest string that can be obtained by replacing '?' ﻿characters such
+ * that substr appears at least once. If it is not possible to do so,﻿return "-1".
+ * Note:
+ * A substring is a coptiguous sequence of characters within a string. For example, "bcd" is a 
+ * substring of "abcde" but "ac" is not.
+ * For two strings a and 𝑏 ﻿of the same length, 𝑎 ﻿is lexicographically smaller than 𝑏 if ai<b𝑖 for some
+ * 0<|𝑎|𝑎𝑗=𝑏𝑗 
+ * word = 𝑎𝑠?𝑏?𝑒?𝑔𝑓 
+ * substr = 𝑑𝑏𝑘.
+ * Replace 3𝑟𝑑 and 5𝑡ℎ dbk 0≤𝑗.
+ * Example word = 𝑎𝑠?𝑏?𝑒?𝑔𝑓 substr =𝑑𝑏𝑘.Replace the 3𝑟𝑑 and 5𝑡ℎ characters with '𝑑' ﻿and '𝑘' 𝑡𝑜 
+ * get "asdbke?𝑔𝑓" ﻿which has substr ="𝑑𝑏𝑘" 𝑎𝑠 𝑎 substring. Replace the remaining '?' with '𝑎'.
+ * The final string 𝑖𝑠 "asdbkeagf".The answer 𝑖𝑠 ﻿"asdbkeagf", without quotes.
+ */
+package com.javaimplant.codingProblems.ibm;
+
+public class LexicographicallySmallestString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
