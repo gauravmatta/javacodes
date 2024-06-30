8e51066cdad7d938be7e1a3d74b3244c4191a517
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-30 13:55:33 +0530

    Added Domain Name System

---
+++ CodingProblems/src/com/javaimplant/codingProblems/ibm/DomainNameSystem.java
@@ -0,0 +1,14 @@
+/*
+ * Given a list of n URLs visited as an array of strings, urls, 
+ * determine the minimum time taken to resolve each DNS request. in Java
+ */
+package com.javaimplant.codingProblems.ibm;
+
+public class DomainNameSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
