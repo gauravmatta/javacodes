99e8e1ab483e49dc8211bf60e8f2c13ef68994c8
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-14 22:08:13 +0530

    Added Flat Map Example

--- PojoClasses/src/javaimplant/exceptions/ClassCastExceptionImpl.java
+++ PojoClasses/src/javaimplant/exceptions/ClassCastExceptionImpl.java
@@ -4,13 +4,10 @@ interface Animal {}
 class Dog implements Animal{}
 class Cat implements Animal{}
 public class ClassCastExceptionImpl {
-	
-	
 	public static void main(String[] args) {
 		Animal dog = new Dog();
 		Animal cat = new Cat();
 		Cat morphedCat = (Cat) dog;
 		System.out.println("Tried creating Morphed Cat");
 	}
-
 }
