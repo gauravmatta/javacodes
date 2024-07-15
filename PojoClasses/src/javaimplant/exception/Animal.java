9d884a2e0bc39cb34edb67ab8cce9102f3af271e
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-15 23:50:28 +0530

    Added Exception in Sub Class and Parent Class

---
+++ PojoClasses/src/javaimplant/exception/Animal.java
@@ -0,0 +1,20 @@
+package javaimplant.exception;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Animal {
+	String name;
+	
+	public void issueUnchecked() throws FileNotFoundException {
+		throw new FileNotFoundException();
+	}
+	
+	public void issueChecked() throws IOException {
+		throw new IOException();
+	}
+	
+	public void issue() {
+		
+	}
+}
