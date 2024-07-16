7ccf46f4a8d025f16e2359c2ea325baad2d62f4a
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-16 14:20:55 +0530

    added Static Synchronization

--- multithreading/src/com/javaimplant/synchronizedstatic/BookSeatMain.java
+++ multithreading/src/com/javaimplant/synchronizedstatic/BookSeatMain.java
@@ -8,5 +8,11 @@ public class BookSeatMain {
 		new Thread(t1).start();
 		BookSeats2 t2 = new BookSeats2(b1,6);
 		new Thread(t2).start();
+		
+		BookTheaterApp b2 = new BookTheaterApp();
+		BookSeats t3 = new BookSeats(b2,7);
+		new Thread(t3).start();
+		BookSeats2 t4 = new BookSeats2(b2,6);
+		new Thread(t4).start();
 	}
 }
--- multithreading/src/com/javaimplant/synchronizedstatic/BookTheaterApp.java
+++ multithreading/src/com/javaimplant/synchronizedstatic/BookTheaterApp.java
@@ -1,8 +1,8 @@
 package com.javaimplant.synchronizedstatic;
 
 public class BookTheaterApp {
-	int total_seats=10;
-	synchronized void bookSeat(int seats) {
+	static int total_seats=10;
+	synchronized static void bookSeat(int seats) {
 		if(total_seats>=seats) {
 			System.out.println("Seats Booked Succesfully");
 			total_seats=total_seats-seats;
