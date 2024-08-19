2588ee6fb5bd1315c5f98fc1d74f790cb75755d6
gauravmatta <gaurav.matta.tech@gmail.com>
2024-08-19 12:31:58 +0530

    Added Static Memory Leak Example

---
+++ PojoClasses/src/javaimplant/memoryleaks/TestStaticMemoryLeaks.java
@@ -0,0 +1,23 @@
+package javaimplant.memoryleaks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestStaticMemoryLeaks {
+	
+	public static List<Double> list = new ArrayList<>();
+	
+	public void populateList() {
+		for(int i=0;i<10000000;i++) {
+			list.add(Math.random());
+		}
+		System.out.println("Check Point 2");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Check Point 1");
+		new TestStaticMemoryLeaks().populateList();
+		System.out.println("Check Point 3");
+	}
+
+}
