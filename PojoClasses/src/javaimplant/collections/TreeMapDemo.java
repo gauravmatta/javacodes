bb8ad074c6b5fe62a080c23972aae0d859d2ce30
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-10-21 17:00:02 +0530

    added TreeMap

--- PojoClasses/src/javaimplant/collections/TreeMapDemo.java
+++ PojoClasses/src/javaimplant/collections/TreeMapDemo.java
@@ -10,6 +10,11 @@ public class TreeMapDemo {
 		treeMap.put(2,"B");
 		treeMap.put(1,"C");
 		System.out.println(treeMap);
+		TreeMap<String,Integer> treeMap1 = new TreeMap<>();
+		treeMap1.put("A",3);
+		treeMap1.put("B",2);
+		treeMap1.put("C",3);
+		System.out.println(treeMap1);
 	}
 
 }
