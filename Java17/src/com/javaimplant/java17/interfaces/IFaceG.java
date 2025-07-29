c48c1bae09c36305c8d4ea398227a3a790f59e68
gauravmatta <gaurav.matta.tech@gmail.com>
2025-07-30 01:20:33 +0530

    void main inside interface

--- Java17/src/com/javaimplant/java17/interfaces/IFaceG.java
+++ Java17/src/com/javaimplant/java17/interfaces/IFaceG.java
@@ -1,14 +1,13 @@
 package com.javaimplant.java17.interfaces;
 
-public interface IFaceG extends IFaceE{
-    public void retY();
-    public void retZ();
+public interface IFaceG {
 
     public static void main(String[] args){
         System.out.println("I am void main inside interface");
         testMe();
-        C c = new C();
-        c.testMeA();
+//        C c = new C();
+//        c.testMeA();
+        IFaceG a=()->System.out.println("My Abstract Method");
     }
 
     private static void testMe(){
@@ -19,21 +18,8 @@ public interface IFaceG extends IFaceE{
         System.out.println("Default Method");
     }
 
-    class C implements IFaceG{
-
-        @Override
-        public void retY() {
-
-        }
-
-        @Override
-        public void retZ() {
-
-        }
-
-        @Override
-        public void method(String a, String b) {
-
-        }
-    }
+//    class C implements IFaceG{
+//        void testMeB(){}
+//    }
+    void testMeB();
 }
