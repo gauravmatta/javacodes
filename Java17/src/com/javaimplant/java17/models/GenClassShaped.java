13412587d7cb8ac3140ab0de5a2c1a90bf8bbe7e
gauravmatta <gaurav.matta.tech@gmail.com>
2025-11-12 18:05:35 +0530

    Added generics upperbound example

--- Java17/src/com/javaimplant/java17/models/GenClassShaped.java
+++ Java17/src/com/javaimplant/java17/models/GenClassShaped.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 public class GenClassShaped {
     public static void draw3dShape(List<? extends BaseShape> l) {
-        l.stream().forEach(n->{
-            n.paintMe();
-        });
+        l.forEach(BaseShape::paintMe);
     }
 }
