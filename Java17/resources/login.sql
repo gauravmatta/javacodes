fd021e39aa1fa4970eebb9649ab1debe58d2e6c5
gauravmatta <gaurav.matta.tech@gmail.com>
2025-11-12 20:57:51 +0530

    Added database

--- Java17/resources/login.sql
+++ Java17/resources/login.sql
@@ -11,3 +11,47 @@ CREATE PROCEDURE deltajavad.splogins()
 BEGIN
 	SELECT * from Logins;
 END
+
+-- deltajavad.Books definition
+
+CREATE TABLE `Books` (
+  `id` int DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- deltajavad.Logins definition
+
+CREATE TABLE `Logins` (
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- deltajavad.Persons definition
+
+CREATE TABLE `Persons` (
+  `pid` int DEFAULT NULL,
+  `pname` varchar(100) DEFAULT NULL,
+  `pemail` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- deltajavad.persons_books definition
+
+CREATE TABLE `persons_books` (
+  `pid` int DEFAULT NULL,
+  `bid` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- deltajavad.Products definition
+
+CREATE TABLE `Products` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `ucost` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
