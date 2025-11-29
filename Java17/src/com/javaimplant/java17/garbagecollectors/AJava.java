87e51715590ec207a7588fae0cfba216f295d1e0
gauravmatta <gaurav.matta.tech@gmail.com>
2025-11-30 02:57:09 +0530

    Added Garbage Collectors dumps

--- Java17/src/com/javaimplant/java17/garbagecollectors/AJava.java
+++ Java17/src/com/javaimplant/java17/garbagecollectors/AJava.java
@@ -1,7 +1,7 @@
 package com.javaimplant.java17.garbagecollectors;
 
 public class AJava {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello, Java 17 Garbage Collectors!");
         for(int i=0;i<100000;i++) {
             String temp = "String number " + i;
