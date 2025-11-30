42e92d5ac37ebcab29aa99528c830492b893410a
gauravmatta <gaurav.matta.tech@gmail.com>
2025-11-30 12:31:04 +0530

    Updated classes

--- Java17/src/com/javaimplant/java17/garbagecollectors/BJava.java
+++ Java17/src/com/javaimplant/java17/garbagecollectors/BJava.java
@@ -1,11 +1,11 @@
 package com.javaimplant.java17.garbagecollectors;
 
-public class AJava {
+public class BJava {
     public static void main(String[] args) {
         System.out.println("Hello, Java 17 Garbage Collectors!");
-        for(int i=0;i<10000000;i++) {
-            String temp = "String number " + i;
-            System.out.println(temp);
+        for(int i=0;i<1000000000;i++) {
+            StringBuffer buf = new StringBuffer("String number " + i);
+            System.out.println(buf);
         }
     }
 }
